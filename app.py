e83b6b3721ed257f413096ab13d7723be2fb1d4f
Sagie86 <sagie.ighodaro@yahoo.co.uk>
2020-06-30 19:50:09 +0000

    change in app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'cooking_receipe_center'
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI", None)
 
 mongo = PyMongo(app)
 
